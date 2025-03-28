a3170be2c2b612bc8f3edba7a3c71eaecbe117e1
pedrocampos <engpedrocampos@gmail.com>
2025-03-27 23:37:25 -0300

    feat: implement rate limiting

--- src/main/java/com/hyperativa/visa/VisaApplication.java
+++ src/main/java/com/hyperativa/visa/VisaApplication.java
@@ -2,8 +2,10 @@ package com.hyperativa.visa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VisaApplication {
 
 	public static void main(String[] args) {
